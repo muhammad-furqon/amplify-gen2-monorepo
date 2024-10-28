00404fa900e791f4b72c7cb11f1041379e9a720f
muhammad-furqon <muhammad.furqon@cosmedia.co.jp>
2024-10-28 14:46:48 +0900

    Edit auth verification email

--- packages/my-shared-backend/amplify/auth/resource.ts
+++ packages/my-shared-backend/amplify/auth/resource.ts
@@ -6,7 +6,17 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      // can be used in conjunction with a customized welcome email as well
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to my app!",
+      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
+      // userInvitation: {
+      //   emailSubject: "Welcome to my app!",
+      //   emailBody: (user, code) =>
+      //     `We're happy to have you! You can now login with username ${user()} and temporary password ${code()}`, 
+      // },
+    },
   },
 
   groups: ["ADMINS", "USERS"],
